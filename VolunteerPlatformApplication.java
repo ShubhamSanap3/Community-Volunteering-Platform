5c643aded74fde79bff184c81cb2c27433685b93
Shubham Sanap <shubhamsanap0306@gmail.com>
2025-01-06 10:28:40 +0530

    Create VolunteerPlatformApplication.java

---
+++ VolunteerPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.example.volunteer_platform;
+import java.lang.String;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerPlatformApplication.class, args);
+	}
+
+}
